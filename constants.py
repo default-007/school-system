68f24663c9f182690fcfab5840ee5e4ce1a2ad8c
default-007 <brianokola@gmail.com>
2023-02-04 20:54:09 +0300

    divide project into apps

---
+++ constants.py
@@ -0,0 +1,62 @@
+from django.utils.translation import gettext_lazy as _
+
+FIRST = "First"
+SECOND = "Second"
+THIRD = "Third"
+TERM = (
+    (FIRST, "First"),
+    (SECOND, "Second"),
+    (THIRD, "Third"),
+)
+
+
+CASH = "Cash"
+CHEAQUE = "Cheaque"
+MPESA = "Mpesa"
+
+PAYMENT_METHOD = ((CASH, "Cash"), (CHEAQUE, "Cheaque"), (MPESA, "Mpesa"))
+
+PAID = "Paid"
+NOT_PAID = "Not Paid"
+PARTIALLY_PAID = "Partially Paid"
+PAID_EXCESS = "Paid Excess"
+
+PAYMENT_STATUS = (
+    (PAID, "Fully Paid"),
+    (NOT_PAID, "Not Paid"),
+    (PARTIALLY_PAID, "Partially Paid"),
+    (PAID_EXCESS, "Paid Excess"),
+)
+
+MALE = "Male"
+FMALE = "Female"
+
+GENDER = ((MALE, "Male"), (FMALE, "Female"))
+
+
+A = "A"
+B = "B"
+C = "C"
+D = "D"
+E = "E"
+F = "F"
+
+GRADE = (
+    (A, "A"),
+    (B, "B"),
+    (C, "C"),
+    (D, "D"),
+    (E, "E"),
+    (F, "F"),
+)
+
+
+DRAFT = "D"
+DELIVERED = "S"
+PENDING = "P"
+
+STATUS = (
+    (DRAFT, _("Draft")),
+    (DELIVERED, _("Delivered")),
+    (PENDING, _("Pending")),
+)
